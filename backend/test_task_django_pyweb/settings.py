4942674982d1d482f1aa783008a3c33d90d25112
Віктор Коропецький <viktorua25.03@gmail.com>
2023-04-06 23:43:52 +0300

    reverted database

--- backend/test_task_django_pyweb/settings.py
+++ backend/test_task_django_pyweb/settings.py
@@ -77,6 +77,10 @@ WSGI_APPLICATION = 'test_task_django_pyweb.wsgi.app'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
