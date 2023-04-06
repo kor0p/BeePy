c3dfcdbeea3e4abdc79a70185b7e2ceec11a67b0
Віктор Коропецький <viktorua25.03@gmail.com>
2023-04-06 23:32:54 +0300

    fixed static dir

--- backend/test_task_django_pyweb/settings.py
+++ backend/test_task_django_pyweb/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
