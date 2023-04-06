f61d0c534ac03d1e4d3dd62f6ef64a73b18e47a1
Віктор Коропецький <viktorua25.03@gmail.com>
2023-04-06 23:31:28 +0300

    change base dir

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python backend/manage.py migrate
-web: gunicorn backend.test_task_django_pyweb.wsgi -b 0.0.0.0:$PORT
+release: cd backend && python manage.py migrate
+web: cd backend && gunicorn test_task_django_pyweb.wsgi -b 0.0.0.0:$PORT
--- backend/test_task_django_pyweb/settings.py
+++ backend/test_task_django_pyweb/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from .utils import IS_DEV
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
